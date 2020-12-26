2596acbb16538934a88326058dd916a568cf141b
Andre Vilela <andrevibegoogle@hotmail.com>
2020-12-26 20:09:20 -0300

    Project created

---
+++ src/main/java/com/andre/challenge/ChallengeZupApplication.java
@@ -0,0 +1,13 @@
+package com.andre.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andre/challenge/ChallengeZupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
